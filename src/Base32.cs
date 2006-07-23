f2178db93efa4f690eb5cec417a26be12573a7fc
lhoerste <lhoerste@committer.creativecommons.org>
2006-07-23 22:00:59 +0000

    Updated license information to conform to the LGPL recommendations

--- src/Base32.cs
+++ src/Base32.cs
@@ -1,14 +1,29 @@
 /***************************************************************************
  *  Base32.cs
  *
+ *  cc-sharp is a library to verify Creative Commons license metadata.
  *  Copyright (C) 2006 Luke Hoersten
- *  Licensed under GNU LGPL (http://www.opensource.org/licenses/lgpl-license.php)
  *  Written by Luke Hoersten <luke.hoersten@gmail.com>
  *
- *  "Encode" method based on Java code by Robert Kaye and Gordon Mohr
+ *  The "Encode" method is based on Java code by Robert Kaye and Gordon Mohr
  *  Public Domain (PD) 2006 The Bitzi Corporation (http://bitzi.com/publicdomain)
  *  (RFC http://www.faqs.org/rfcs/rfc3548.html)
  ****************************************************************************/
+ 
+/*  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
  
 using System;
 using System.IO;
